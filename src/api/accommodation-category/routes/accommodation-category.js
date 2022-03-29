7a5a428bf61439751c3f680562de1c4772683208
Loris Birkemeyer <loris.birkemeyer@edu.gobelins.fr>
2022-03-29 14:29:25 +0200

    feat: delete categories of accommodation and activities

--- src/api/accommodation-category/routes/accommodation-category.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * accommodation-category router.
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::accommodation-category.accommodation-category');
